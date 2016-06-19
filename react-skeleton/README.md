3c886086cf3dc56bd79cbb64d4ed7a5d80ea24bb
walter.li <hi55216@gmail.com>
2016-06-19 18:41:28 +0800

    react-skeleton and simple list items

---
+++ react-skeleton/README.md
@@ -0,0 +1,8 @@
+build react skeleton
+1. npm init
+2. (global) npm install -g browserify
+3. npm install --save babelify
+4. npm install --save watchify
+5. npm install --save babel-preset-react
+6. npm install --save react
+7. npm install --save react-dom
---
+++ react-skeleton/src/components/List.jsx
@@ -0,0 +1,20 @@
+var React = require('react');
+var ListItem = require('./ListItem.jsx');
+
+var ingredient = [
+  {"id":1, "text": "ham"},
+  {"id":2, "text": "cheese"},
+  {"id":3, "text": "jomi"}
+];
+
+var List = React.createClass({
+  render: function(){
+    var listItems = ingredient.map(function(item){
+      return <ListItem key={item.id} ingredient={item.text} />;
+    });
+
+    return (<ul>{listItems}</ul>)
+  }
+});
+
+module.exports = List;
